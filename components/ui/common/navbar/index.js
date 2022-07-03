9d1c995154fab171331dacb314c0894be0ead041
Andrew Sokolenko <a5okolwork@gmail.com>
2022-07-03 20:14:27 +0400

    MP-1 create UI and web3

---
+++ components/ui/common/navbar/index.js
@@ -0,0 +1,65 @@
+import Link from 'next/link';
+
+import { useWeb3 } from '@components/providers';
+import { Button } from '@components/ui/common';
+import { useAccount } from '@components/web3/hooks/useAccount';
+
+const Navbar = () => {
+  const { connect, isLoading, isWeb3Loaded } = useWeb3();
+  const { account } = useAccount();
+
+  const isConnected = isWeb3Loaded ? (
+    <Button onClick={connect}>Connect </Button>
+  ) : (
+    <Button
+      onClick={() => window.open('https://metamask.io/download.html', '_blank')}
+    >
+      Install Metamask
+    </Button>
+  );
+
+  return (
+    <section>
+      {account}
+      <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+        <nav className="relative" aria-label="Global">
+          <div className="flex justify-between items-center">
+            <div>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Home
+                </a>
+              </Link>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Marketplace
+                </a>
+              </Link>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Blogs
+                </a>
+              </Link>
+            </div>
+            <div>
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                  Wishlist
+                </a>
+              </Link>
+              {isLoading ? (
+                <Button onClick={connect} disabled={true}>
+                  Loading...
+                </Button>
+              ) : (
+                isConnected
+              )}
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
