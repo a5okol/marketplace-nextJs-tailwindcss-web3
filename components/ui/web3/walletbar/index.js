96770a3c9320f54f25e1c532f0bc7d7c6da4b6ed
Andrew Sokolenko <a5okolwork@gmail.com>
2022-08-20 14:14:54 +0400

    MP-2 Network Hook and Order

--- components/ui/web3/walletbar/index.js
+++ components/ui/web3/walletbar/index.js
@@ -1,10 +1,12 @@
-const WalletBar = () => {
+import { useWeb3 } from '@components/providers';
+
+const WalletBar = ({ address, network }) => {
+  const { requireInstall } = useWeb3();
+
   return (
-    <section className="text-white bg-indigo-600">
+    <section className="text-white bg-indigo-600 rounded-lg">
       <div className="p-8">
-        <h1 className="text-2xl">
-          Hello, 0xd9D5cD41Fe921A743F2b5Fe71CC3070F5C176208
-        </h1>
+        <h1 className="text-2xl">Hello, {address}</h1>
         <h2 className="subtitle mb-5 text-xl">
           I hope you are having a great day!
         </h2>
@@ -20,10 +22,26 @@ const WalletBar = () => {
             </div>
           </div>
           <div>
-            <div>
-              <span>Currently on </span>
-              <strong className="text-2xl">Ethereum Main Network</strong>
-            </div>
+            {network.hasInitialResponse && !network.isSupported && (
+              <div className="bg-red-400 p-4 rounded-lg">
+                <div>Connected to wrong network</div>
+                <div>
+                  Connect to: {` `}
+                  <strong className="text-2xl">{network.target}</strong>
+                </div>
+              </div>
+            )}
+            {requireInstall && (
+              <div className="bg-yellow-500 p-4 rounded-lg">
+                Cannot connect to network. Please install Metamask.
+              </div>
+            )}
+            {network.data && (
+              <div>
+                <span>Currently on </span>
+                <strong className="text-2xl">{network.data}</strong>
+              </div>
+            )}
           </div>
         </div>
       </div>
