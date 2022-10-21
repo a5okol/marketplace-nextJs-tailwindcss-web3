f59bbb3aa0f71693ea9b1527227f8ce9ad336ffd
Andrew Sokolenko <a5okolwork@gmail.com>
2022-10-21 22:41:25 +0400

    MP-8 Style Improvements

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -29,9 +29,9 @@ const Navbar = () => {
 
   return (
     <section>
-      <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+      <div className="relative pt-6 px-4 sm:px-6 lg:pl-8 lg:pr-0">
         <nav className="relative" aria-label="Global">
-          <div className="flex justify-between items-center">
+          <div className="flex flex-col xs:flex-row justify-between items-center">
             <div>
               <ActiveLink href="/">
                 <a className="font-medium mr-8 hover:text-gray-900">Home</a>
@@ -45,9 +45,11 @@ const Navbar = () => {
                 <a className="font-medium mr-8 hover:text-gray-900">Blogs</a>
               </ActiveLink>
             </div>
-            <div>
+            <div className="text-center">
               <ActiveLink href="/wishlist">
-                <a className="font-medium mr-8 hover:text-gray-900">Wishlist</a>
+                <a className="font-medium sm:mr-8 mr-1 text-gray-500 hover:text-gray-900">
+                  Wishlist
+                </a>
               </ActiveLink>
               {isLoading ? (
                 <Button onClick={connect} disabled={true}>
